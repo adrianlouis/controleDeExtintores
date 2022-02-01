1e54052502c0db309f516382d2b6c7cf2608bf90
AdrianLouis <adrian.louis.003@live.com>
2022-02-01 16:15:34 -0300

    modal atualizado

--- service/ext-service.js
+++ service/ext-service.js
@@ -81,6 +81,7 @@ http.onload = () => {
 
 function horus(codigo){
     const modal = document.querySelector('.modalExtFull')
+    var codigoUniversal = codigo
     modal.style.left = 0;
     // let array = document.getElementsByTagName('h2')
     // console.log(array.innerHTML)
@@ -90,19 +91,38 @@ function horus(codigo){
     http.onload = () => {
         const data = JSON.parse(http.response)
 
-        // ultimaRecarga = 
+        let ultimaRec = new Date(data.ultimaRecarga)
+        ultimaRec = ultimaRec.toLocaleDateString('pt-PT').split('/').reverse().join('-')
+        let ultimoRet = new Date(data.ultimoReteste)
+        ultimoRet = ultimoRet.toLocaleDateString('pt-PT').split('/').reverse().join('-')
+        let proxRec = new Date (data.proximaRecarga)
+        proxRec = proxRec.toLocaleDateString('pt-PT').split('/').reverse().join('-')
+        let proxRet = new Date (data.proximoReteste)
+        proxRet = proxRet.toLocaleDateString('pt-PT').split('/').reverse().join('-')
 
     document.querySelector('#extintorNumero').innerHTML = data.numero;
     document.querySelector('#numeroExtintor').value = data.numero
     document.querySelector('#tipo').value = data.tipo
     document.querySelector('#local').value = data.codigoSubLocal
-    // document.querySelector('#ultRec').value = data.ultimaRecarga.toLocaleDateString('pt-PT');
-    // document.querySelector()
-    // document.querySelector()
-    // document.querySelector()
-    // document.querySelector()
-}
-}
+    document.querySelector('#ultRec').value = ultimaRec
+    document.querySelector('#ultRet').value = ultimoRet
+    document.querySelector('#proxRec').value = proxRec
+    document.querySelector('#proxRet').value = proxRet
+    document.querySelector('#avaria').innerHTML = data.avarias
+
+    let type = document.querySelector('#tipo')
+    let classeExt = document.querySelector('#classe')
+
+    if (type.value == "0"){
+        classeExt.value = "A"
+    }else if (type.value == "1"){
+        classeExt.value = "C"
+    }else{
+        classeExt.value = "B"
+    }
+    console.log('TIPO value: '+type.value)
+   
+}}
 
 function formEdit(show, hide){
     const inputs = document.getElementsByTagName('input')
@@ -135,3 +155,7 @@ function formEdit(show, hide){
     document.querySelector(show).classList.remove("innactive")
 }
 
+function closeModal(){
+    const modal = document.querySelector('.modalExtFull')
+    modal.style.left = '-101vw';
+}
